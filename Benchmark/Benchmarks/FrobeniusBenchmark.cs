d652f961d193ab669d4687b2e24e8928262df45d
Igor Strunin <struninigor@mail.ru>
2021-03-12 23:06:33 +0300

    Extend Frobenius benchmark samples

--- Benchmark/Benchmarks/FrobeniusBenchmark.cs
+++ Benchmark/Benchmarks/FrobeniusBenchmark.cs
@@ -6,24 +6,59 @@ namespace Benchmark.Benchmarks
 {
     public class FrobeniusBenchmark
     {
-        private const int MatrixSize = 1000;
-        private Matrix<double> _matrix;
+        private const int SmallMatrixSize = 100;
+        private const int MediumMatrixSize = 500;
+        private const int LargeMatrixSize = 1000;
+        private Matrix<double> _smallMatrix;
+        private Matrix<double> _mediumMatrix;
+        private Matrix<double> _largeMatrix;
 
         public FrobeniusBenchmark()
         {
-            _matrix = Matrix<double>.Build.RandomSquare(MatrixSize);
+            _smallMatrix = CreateRandomMatrix(SmallMatrixSize);
+            _mediumMatrix = CreateRandomMatrix(MediumMatrixSize);
+            _largeMatrix = CreateRandomMatrix(LargeMatrixSize);
         }
 
         [Benchmark]
-        public void FrobeniosOneThread()
+        public void FrobeniosOneThreadSmall()
         {
-            Algorithm.OneThreadFrobenius(_matrix);
+            Algorithm.OneThreadFrobenius(_smallMatrix);
         }
 
         [Benchmark]
-        public void FrobeniosMultiThread()
+        public void FrobeniosMultiThreadSmall()
         {
-            Algorithm.MultiThreadFrobenius(_matrix);
+            Algorithm.MultiThreadFrobenius(_smallMatrix);
+        }
+        
+        [Benchmark]
+        public void FrobeniosOneThreadMedium()
+        {
+            Algorithm.OneThreadFrobenius(_mediumMatrix);
+        }
+
+        [Benchmark]
+        public void FrobeniosMultiThreadMedium()
+        {
+            Algorithm.MultiThreadFrobenius(_mediumMatrix);
+        }
+
+        [Benchmark]
+        public void FrobeniosOneThreadLarge()
+        {
+            Algorithm.OneThreadFrobenius(_largeMatrix);
+        }
+
+        [Benchmark]
+        public void FrobeniosMultiThreadLarge()
+        {
+            Algorithm.MultiThreadFrobenius(_largeMatrix);
+        }
+
+        private Matrix<double> CreateRandomMatrix(int size)
+        {
+            return Matrix<double>.Build.RandomSquare(size);
         }
     }
 }
