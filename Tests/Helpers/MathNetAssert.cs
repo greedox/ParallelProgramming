52aa79627efcd123bbd9e8903b8c6471a8aed412
Igor Strunin <struninigor@mail.ru>
2021-03-13 13:47:58 +0300

    Add unit tests for Frobenius algorithm

---
+++ Tests/Helpers/MathNetAssert.cs
@@ -0,0 +1,24 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using Xunit;
+
+namespace Tests.Helpers
+{
+    public class MathNetAssert
+    {
+        public static void Equal(Matrix<double> expected, Matrix<double> actual, int precision = 15)
+        {
+            Assert.True(expected.RowCount == actual.RowCount);
+            Assert.True(expected.ColumnCount == actual.ColumnCount);
+            for (int row = 0; row < expected.RowCount; row++)
+            {
+                for (int column = 0; column < expected.ColumnCount; column++)
+                {
+                    double expectedValue = expected[row, column];
+                    double actualValue = actual[row, column];
+
+                    Assert.Equal(expectedValue, actualValue, precision);
+                }
+            }
+        }
+    }
+}
